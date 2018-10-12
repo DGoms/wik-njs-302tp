1da25691654d8fb9299ce15f5fa7ad5922a9df2c
goms <dgomesh@gmail.com>
2018-10-12 12:41:48 +0200

    base model OK

--- core/BaseModel.ts
+++ core/BaseModel.ts
@@ -1,11 +1,16 @@
 import * as urljoin from "urljoin";
 import api from '../api';
 
-export abstract class BaseModel<T> {
-protected static basePath: string = '';
+export class BaseModel<T> {
+    protected static basePath: string = '';
+    protected static availableIncludes: string[];
 
-    constructor(private basePath) {
-        
+    constructor(data: T) {
+        Object.assign(this, data);
+    }
+
+    protected static getType(): typeof BaseModel {
+        return BaseModel;
     }
 
     protected static getFullUrl(endUrl: string|number): string {
@@ -13,18 +18,20 @@ protected static basePath: string = '';
             endUrl = String(endUrl);
         }
 
-        return urljoin()
+        return urljoin(this.basePath, endUrl);
     }
 
-    static async find(filter: T) {
+    static async find(filter) {
         
     }
 
-    static async findById(id: number){
-        const { data } = await api.get(this.getFullUrl(id));
+    static async findById<U>(id: number, includes?: string[]): Promise<U>{
+        const { data } = await api.get<U>(this.getFullUrl(id));
         const model = new this(data);
-        if (includes && includes.length) await album.loadIncludes(includes)
-        return album
+        // if (includes && includes.length) await album.loadIncludes(includes)
+
+        // @ts-ignore
+        return model;
     }
 
     create() {
--- models/album.ts
+++ models/album.ts
@@ -4,51 +4,44 @@ import api from '../api';
 import { BaseModel } from "../core/BaseModel";
 
 export interface AlbumSchema {
-  userId: number
-  id: number
-  title: string
+    userId: number
+    id: number
+    title: string
 }
 
 export class Album extends BaseModel<AlbumSchema> implements AlbumSchema {
-  basePath = 'albums';
+    protected static basePath: string = 'albums';
+    protected static availableIncludes: string[] = ['user', 'photos'];
 
-  userId: number
-  id: number
-  title: string
+    userId: number
+    id: number
+    title: string
 
-  user: UserSchema | void = null
-  photos: PhotoSchema[] = []
+    user: UserSchema | void = null
+    photos: PhotoSchema[] = []
 
-  constructor(albumData: AlbumSchema) {
-    super(['user', 'photos']);
-    Object.assign(this, albumData)
-  }
+    constructor(albumData: AlbumSchema) {
+        super(albumData);
+    }
 
-  static async findById(albumId: number, includes?: string[]): Promise<Album> {
-    const { data } = await api.get<AlbumSchema>(`albums/${albumId}`)
-    const album = new Album(data)
-    if (includes && includes.length) await album.loadIncludes(includes)
-    return album
-  }
+    // async loadIncludes(includes: string[]): Promise<void> {
+    //     await Promise.all(includes.map(async (include) => {
+    //         switch (include) {
+    //             case 'user':
+    //                 const { data: userData } = await api.get<UserSchema>(`users/${this.userId}`)
+    //                 this.user = userData
+    //                 break
+    //             case 'photos':
+    //                 const { data: photoData } = await api.get<PhotoSchema[]>(`photos?albumId=${this.id}`)
+    //                 this.photos = photoData
+    //                 break
+    //         }
+    //     }))
+    // }
 
-  async loadIncludes(includes: string[]): Promise<void> {
-    await Promise.all(includes.map(async (include) => {
-      switch (include) {
-        case 'user':
-          const { data: userData } = await api.get<UserSchema>(`users/${this.userId}`)
-          this.user = userData
-          break
-        case 'photos':
-          const { data: photoData } = await api.get<PhotoSchema[]>(`photos?albumId=${this.id}`)
-          this.photos = photoData
-          break
-      }
-    }))
-  }
-
-  toString() {
-    return JSON.stringify(this)
-  }
+    toString() {
+        return JSON.stringify(this)
+    }
 }
 
 export default Album
